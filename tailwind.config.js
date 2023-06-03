ae39655e2038cf8ed88d06f814d77716921d3da6
Robert.minia <omid-bakeri>
2023-06-03 10:08:52 +0430

    file updated navigator completed

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["iransans_medium", "sans-serif"],
+        display: ["iransans_black", "sans-serif"],
+        default: ["iransans_light", "sans-serif"],
+      },
+      colors: {
+        DigiRed: "#f91a47",
+        DigiBlack: "#111827",
+        DigiBlackLight: "#1e293b",
+        DigiWhite: "#f8fafc",
+        DigiInputBG: "#334155",
+      },
+    },
+  },
+  plugins: [],
+};
